9bda603b8414a7f22fc612fb0e34da5ee11b7afe
cayivor24 <csa85@scarletmail.rutgers.edu>
2026-07-07 23:28:34 -0400

    docs: finalize project documentation and reflections

--- tests/test_pawpal.py
+++ tests/test_pawpal.py
@@ -2,10 +2,11 @@ from pawpal_system import Task, Pet
 
 def test_task_completion():
     """Verify that calling mark_complete() actually changes the task's status."""
+    pet = Pet(name="Bella", species="Dog") 
     task = Task(description="Morning Walk", due_time="08:00 AM")
     assert task.is_completed == False
     
-    task.mark_complete()
+    task.mark_complete(pet) 
     
     assert task.is_completed == True
 
